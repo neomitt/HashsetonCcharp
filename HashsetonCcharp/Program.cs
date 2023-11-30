7fb53a9b87c3ca263f44343d5924cb08706cd10f
Georgiy <149246960+neomitt@users.noreply.github.com>
2023-11-30 16:33:10 +0600

    Add files via upload

---
+++ HashsetonCcharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashsetonCcharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            while (true)
+            {
+                
+                Console.WriteLine("1.add 2.output");
+                int an = Int32.Parse(Console.ReadLine());
+                if (an == 1)
+                {
+                    Console.WriteLine("add:");
+                    set.Add(Console.ReadLine());
+                }
+                else if (an == 2)
+                {
+                    foreach(string i in set)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Er");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
